f6bd1bdfd2e06602e159fd65f380b4b45576cc9c
Yehu Carrington <yehu41@gmail.com>
2019-11-11 16:30:25 -0400

    Added in the scroll function to the LCD

--- L02_815008434.X/code_lab2_6.c
+++ L02_815008434.X/code_lab2_6.c
@@ -12,6 +12,8 @@
 #include    <stdlib.h>
 #include    <adc.h>
 #include    <delays.h>
+#include    <string.h>
+#include    <stdio.h>
 
 #pragma config OSC = HS
 #pragma config WDT = OFF
@@ -23,45 +25,42 @@
 SemaphoreHandle_t ADCSem;
 QueueHandle_t ADCQueue;
 QueueHandle_t LCDQueue;
-volatile short int Vread;
-volatile short int Vbuff;
-volatile short int LCDread;
-volatile short int *LCDbuff[4];
-volatile char *dispBuff[4];
+short int Vread;
+short int Vbuff;
+short int LCDread;
+short int LCDbuff;
+char dispBuff[5] = "";
+
 
 /*FUNCTIONS DECLARATIONS*/
+void AnalogTask(void *pvParameter);
+void LCDTask(void *pvParameter);
+void FilterTask(void *pvParameter);
+void ADCisr(void);
 
-void AnalogTask (void *pvParameter);
-void LCDTask (void *pvParameter);
-void FilterTask (void *pvParameter);
-void ADCisr (void);
-
-void main (void)
-{  
+void main(void) {
     INTCONbits.GIE = 0;
     TRISBbits.RB2 = 0;
-    PORTBbits.RB2 = 0; 
-    ADCSem = xSemaphoreCreateCounting(1,0);
-    ADCQueue = xQueueCreate(2, sizeof(short int));
-    LCDQueue = xQueueCreate(2, sizeof(short int));
-    
+    PORTBbits.RB2 = 0;
+    ADCSem = xSemaphoreCreateCounting(1, 0);
+    ADCQueue = xQueueCreate(1, sizeof (short int));
+    LCDQueue = xQueueCreate(1, sizeof (short int));
+
     xTaskCreate(AnalogTask, "AnalogTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     xTaskCreate(LCDTask, "LCDTask", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
     xTaskCreate(FilterTask, "FilterTask", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-    
+
     InitLCD();
     vTaskStartScheduler();
     for (;;);
-    
+
 }
 
-void AnalogTask (void *pvParameter)
-{
+void AnalogTask(void *pvParameter) {
     OpenADC(ADC_FOSC_8 & ADC_RIGHT_JUST & ADC_6ANA_2REF, ADC_CH0 & ADC_INT_ON);
     Delay10TCYx(2);
     ConvertADC();
-    while(1)
-    {
+    while (1) {
         xSemaphoreTake(ADCSem, portMAX_DELAY);
         LATBbits.LATB2 = !LATBbits.LATB2;
         Vread = ADRESL;
@@ -69,84 +68,72 @@ void AnalogTask (void *pvParameter)
         xQueueSend(ADCQueue, &Vread, 0); //The queue should never be full according to the priorities of the task
         ConvertADC();
     }
-   
+
 }
 
-void LCDTask (void * pvParameters)
-{
-    static int lcdCount = 0;
-    while(1)
-    {
-        while(lcdCount < 4)
-        {
-            xQueueReceive(LCDQueue, &LCDbuff[lcdCount], portMAX_DELAY);
-            itoa(LCDbuff[lcdCount], *dispBuff[lcdCount]);
-
-            if(lcdCount == 0)
-            {
-                while (BusyXLCD());
-                
-                SetDDRamAddr(0x00);
-                while (BusyXLCD());
-                putsXLCD(*dispBuff[lcdCount]);
-                while (BusyXLCD());
-            }
-            
-            if(lcdCount == 1)
-            {
-                *dispBuff[lcdCount] = *dispBuff[lcdCount - 1];
-                while (BusyXLCD());
-                SetDDRamAddr(0x40);
-                while (BusyXLCD());
-                putsXLCD(*dispBuff[lcdCount]);
-                while (BusyXLCD());
-                
-            }
-            
-            if(lcdCount == 2)
-            {
-                *dispBuff[lcdCount] = *dispBuff[lcdCount - 1];
-                while (BusyXLCD());
-                SetDDRamAddr(0x10);
-                while (BusyXLCD());
-                putsXLCD(*dispBuff[lcdCount]);
-                while (BusyXLCD());
-            }
-            
-            if(lcdCount == 3)
-            {
-                *dispBuff[lcdCount] = *dispBuff[lcdCount - 1];
-                while (BusyXLCD());
-                SetDDRamAddr(0x50);
-                while (BusyXLCD());
-                putsXLCD(*dispBuff[lcdCount]);
-                while (BusyXLCD());
-            }
-            
-            lcdCount++; 
-        }
-        
-        lcdCount = 0;
-    }
+void LCDTask(void * pvParameters) {
     
+    static char thirdLine[5] = "";
+    static char secondLine[5] = "";
+    static char firstLine[5] = "";
+    static char blank[5] = {32, 32, 32, 32, 32};
+    while (1) {
+        xQueueReceive(LCDQueue, &LCDbuff, portMAX_DELAY);
+        itoa(LCDbuff, dispBuff);
+        while (BusyXLCD());
+        SetDDRamAddr(0x50);
+        while (BusyXLCD());
+        putsXLCD(blank);
+        while (BusyXLCD());
+        SetDDRamAddr(0x50);
+        while (BusyXLCD());
+        putsXLCD(dispBuff);
+        while (BusyXLCD());
+        SetDDRamAddr(0x10);
+        while (BusyXLCD());
+        putsXLCD(blank);
+        while (BusyXLCD());
+        SetDDRamAddr(0x10);
+        while (BusyXLCD());
+        putsXLCD(thirdLine);
+        while (BusyXLCD());
+        SetDDRamAddr(0x40);
+        while (BusyXLCD());
+        putsXLCD(blank);
+        while (BusyXLCD());
+        SetDDRamAddr(0x40);
+        while (BusyXLCD());
+        putsXLCD(secondLine);
+        while (BusyXLCD());
+        SetDDRamAddr(0x00);
+        while (BusyXLCD());
+        putsXLCD(blank);
+        while (BusyXLCD());
+        SetDDRamAddr(0x00);
+        while (BusyXLCD());
+        putsXLCD(firstLine);
+        while (BusyXLCD());
+
+        strncpy(firstLine, secondLine, 5);
+        strncpy(secondLine, thirdLine, 5);
+        strncpy(thirdLine, dispBuff, 5);
+    }
+
     vTaskDelete(NULL);
 }
 
-void FilterTask (void * pvParameters)
-{
+void FilterTask(void * pvParameters) {
     volatile static int filterCount = 0;
     volatile static int tempVal = 0;
-    
-    while(1)
-    {
-        while(filterCount < 10)
-        {
+
+    while (1) {
+        while (filterCount < 10) {
             xQueueReceive(ADCQueue, &Vbuff, portMAX_DELAY);
             tempVal = Vbuff + tempVal;
             filterCount++;
         }
-        
-        LCDread = (tempVal/10);
+
+        LCDread = (tempVal / 10);
         xQueueSend(LCDQueue, &LCDread, 0);
         tempVal = 0;
         filterCount = 0;
@@ -154,10 +141,10 @@ void FilterTask (void * pvParameters)
 }
 
 #pragma interruptlow ADCisr
-void ADCisr ( void )
-{
+
+void ADCisr(void) {
     PIR1bits.ADIF = 0;
-    xSemaphoreGiveFromISR(ADCSem,NULL);
+    xSemaphoreGiveFromISR(ADCSem, NULL);
 }
 
 /*
@@ -168,4 +155,4 @@ void appISR( void ) {
     INTCONbits.TMR0IF = 0;
     xSemaphoreGiveFromISR(TimerSem,NULL);
 }
-*/
+ */
